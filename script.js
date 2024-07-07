dadd2552ad466169e7f7e55eb85fcaf3c1d788f0
ReubenEAE <reubentwj@gmail.com>
2024-07-08 01:02:46 +0800

    fucking helkp

--- script.js
+++ script.js
@@ -1,57 +1,20 @@
-function openTab(evt, tabName) {
+function openCity(evt, cityName) {
+    // Declare all variables
     var i, tabcontent, tablinks;
-
-    // Hide all tab content
+  
+    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+      tabcontent[i].style.display = "none";
     }
-
-    // Deactivate all tab links
+  
+    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].classList.remove("active");
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
-    // Show the specific tab content
-    document.getElementById(tabName).style.display = "block";
-
-    // Activate the current tab link
-    evt.currentTarget.classList.add("active");
-
-    // Show the first slide when switching tabs
-    slideIndex[tabName] = 1;
-    showSlides(slideIndex[tabName], tabName);
-}
-
-// Function to control image slider
-var slideIndex = {};
-
-function plusSlides(n, tabName) {
-    showSlides(slideIndex[tabName] += n, tabName);
-}
-
-function currentSlide(n, tabName) {
-    showSlides(slideIndex[tabName] = n, tabName);
-}
-
-function showSlides(n, tabName) {
-    var i;
-    var slides = document.getElementById(tabName).getElementsByClassName("mySlides");
-    var dots = document.getElementById(tabName).getElementsByClassName("demo");
-
-    if (n > slides.length) {
-        slideIndex[tabName] = 1;
-    }
-    if (n < 1) {
-        slideIndex[tabName] = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex[tabName] - 1].style.display = "block";
-    dots[slideIndex[tabName] - 1].className += " active";
-}
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
